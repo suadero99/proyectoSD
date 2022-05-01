e5cacc02b2c2daea9f4ac40ab7b00559985f0b57
suadero99 <froysantos99@gmail.com>
2022-05-01 13:17:30 -0500

    Mejorada mucho la plantilla principal

--- plantillas/plantilla_principal.php
+++ plantillas/plantilla_principal.php
@@ -23,7 +23,7 @@
     <!--Bootstrap bundle with popper-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     
-    <!--logo y barra de búsqueda-->
+    <!--Lo necesario para Login-->
     <div class="container-fluid my-4">
       <div class="row justify-content-end">
         <div class="col-10">
