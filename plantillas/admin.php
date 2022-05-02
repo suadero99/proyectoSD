6775b2de200bee9d9e70e13e9d73013c1ad1a4b7
suadero99 <froysantos99@gmail.com>
2022-05-02 11:58:42 -0500

    Mejorada la plantilla de estadisticas

--- plantillas/admin.php
+++ plantillas/admin.php
@@ -14,12 +14,61 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
   <div class="container-fluid">
+    <!--Barra de logo, hola ### y foto-->
     <div class="row align-items-center">
       <div class="col-lg-3">
         <img src="gugul_logo_180.png" alt="logo" style="max-width: 180px;">
       </div>
       <div class="col-lg-6">
-        <h1 class="display-6">Panel Admin</h1>
+        <h1 class="display-6">Panel de administrador</h1>
+      </div>
+      <div class="col-lg-1">
+        Hola, usuario
+      </div>
+      <div class="col-lg-2">
+        foto_usuario
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row my-5">
+      <!--Consultas más realizadas-->
+      <div class="col-md-3 text-center">
+        <h5>Consultas más realizadas</h5>
+        <p>1._________________________</p>
+        <p>2._________________________</p>
+        <p>3._________________________</p>
+        <p>4._________________________</p>
+      </div>
+      <!--Estadísticas-->
+      <div class="col-lg-9">
+        <div class="row text-center">
+          <h5>Estadísticas de anuncios</h5>
+        </div>
+        <div class="card-group">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Empresa</h5>
+              <p class="card-text">Nombre de empresa</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <img src="..." class="card-img-top" alt="anuncio mostrado">
+              <h5 class="card-title">Anuncio mostrado</h5>
+              <p class="card-text"></p>
+              <div class="card-footer">
+                <small class="text-muted">Imagen perteneciente al anuncio</small>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Número de clicks</h5>
+              <p class="card-text">#### clicks</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
